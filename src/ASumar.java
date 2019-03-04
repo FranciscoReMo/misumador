83f5f29c025a429067154ca598f3a6db95a9418b
DESKTOP-GL52QQN\Francisco <rerustraven@gmail.com>
2019-03-04 13:18:35 +0100

    Javadoc en ASumar

--- src/ASumar.java
+++ src/ASumar.java
@@ -1,12 +1,21 @@
 
 public class ASumar {
 
+	/**
+	 * Guarda variable
+	 */
     String valor_inicial;
-	
+	/**
+	 * Metodo constructor
+	 * @param string
+	 */
     public ASumar(String string) {
         this.valor_inicial = string;
     }
-    
+    /**
+     * Metodo que realiza la suma y devuelve el numero
+     * @return
+     */
 	public String mostrar() {
         String numero = valor_inicial;
         if (numero.length() == 1) {
