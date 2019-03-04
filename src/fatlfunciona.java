c24f9e3f55a4e4c4a0e271afb8741b5bb785fcbe
DESKTOP-GL52QQN\Francisco <rerustraven@gmail.com>
2019-03-04 13:35:38 +0100

    Funciona

---
+++ src/fatlfunciona.java
@@ -0,0 +1,12 @@
+
+public class fatlfunciona {
+
+	public static void main(String[] args) {
+		/**
+		 * Llamada a ASumar
+		 */
+		ASumar suma = new ASumar(args[0]);
+		System.out.println(suma.mostrar());
+	}
+
+}
